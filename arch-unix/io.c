73bd4c1b2872df0bb6491860026f413ff8614123
Alessandro Rubini <rubini@gnudd.com>
2013-12-21 13:02:27 +0100

    arch-unix: use initcalls
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- arch-unix/io.c
+++ arch-unix/io.c
@@ -4,14 +4,24 @@
  */
 #include <stdint.h>
 #include <bathos/bathos.h>
+#include <bathos/init.h>
 #include <arch/hw.h>
 
 /* We need a main function, called by libc initialization */
 int main(int argc, char **argv)
 {
+	bathos_setup();
 	bathos_main();
 	return 0;
 }
+
+/*
+ * There is no romcall for arch-unix and we have no lds to provide symbols
+ * We strongly hope the linker will continue keeping these at the same address
+ */
+initcall_t  romcall_begin[0];
+initcall_t  romcall_end[0];
+
 
 /* And this trivially uses stdout */
 void putc(int c)
